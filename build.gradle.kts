0bbfe73b78cdfc15a4d786a854b4df9d4cd03f00
Simao Ferreira <simao.ferreira@tuta.io>
2020-07-26 16:31:19 +0200

    BS: Inital commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    groovy
+    java
+    kotlin("jvm") version "1.3.72"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.codehaus.groovy:groovy-all:2.3.11")
+    implementation(kotlin("stdlib-jdk8"))
+    testImplementation("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "1.8"
+    }
+}
